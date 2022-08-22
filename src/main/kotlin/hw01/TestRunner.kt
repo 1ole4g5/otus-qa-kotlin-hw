2f16df91e743a5d754262d9e1eec948bd10f478c
Oleg Martynenko <qa@m-i-f.ru>
2022-08-22 20:52:11 +0300

    Fixed unused var steps.

--- src/main/kotlin/hw01/TestRunner.kt
+++ src/main/kotlin/hw01/TestRunner.kt
@@ -1,18 +1,23 @@
 package hw01
 
+import kotlin.reflect.full.functions
+
 interface TestRunner<T> {
     fun runTest(steps: T, test: () -> Unit)
 }
 
 class MyTestRunner : TestRunner<Any> {
+
     override fun runTest(steps: Any, test: () -> Unit) {
-        Steps().before()
+        steps::class.functions.filter { it.name.contains("before") }.forEach { it.call(steps) }
         test()
-        Steps().after()
+        steps::class.functions.filter { it.name.contains("after") }.forEach { it.call(steps) }
     }
 }
 
 class Steps() {
+    fun afterAll() = log("afterAll test")
+
     fun before() {
         log("before test")
     }
